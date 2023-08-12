1a9cbb862839842ba4072ef4e53b30170be18afd
Beatriz Moreli <beatrizmoreli.01@gmail.com>
2023-08-11 21:01:21 -0300

    Front End Produtos

---
+++ src/components/Formulario.js
@@ -0,0 +1,18 @@
+function Formulario() {
+    return (
+
+        <form>
+            <input type="text" placeholder='Nome' />
+            <input type="text" placeholder="Marca" />
+
+            <input type="button" value="Alterar" />
+            <input type="button" value="Deletar" />
+            <input type="button" value="Cancelar" />
+            <input type="button" value="Cadastrar" />
+
+        </form>
+    );
+}
+
+
+export default Formulario;
