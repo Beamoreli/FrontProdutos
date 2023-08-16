bc111d61c2b3f7acb81d39738ccd70387c27f8c8
Beatriz Moreli <beatrizmoreli.01@gmail.com>
2023-08-16 17:21:31 -0300

    Adcionando complementos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,124 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './components/Formulario';
 import Tabela from './components/Tabela';
 
 function App() {
+
+  const produto = {
+    codigo: 0,
+    nome: '',
+    marca: ''
+  }
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjtProduto] = useState(produto);
+
+
+
+  useEffect(() => {
+    fetch("http://localhost:8080/listar").then(retorno => retorno.json()).then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+  const aoDigitar = (e) => {
+    setObjtProduto({ ...objProduto, [e.target.name]: e.target.value });
+  }
+
+  const cadastrar = () => {
+    fetch("http://localhost:8080/cadastrar", {
+      method: "post",
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(retorno => retorno.json()).then(retorno_convertido => {
+        if (retorno_convertido.mensagem !== undefined) {
+          alert(retorno_convertido.mensagem);
+
+        } else {
+          setProdutos([...produtos, retorno_convertido]);
+          alert('Produto Cadastrado com Sucesso');
+          limparFormulario();
+        }
+      })
+  }
+
+  const limparFormulario = () => {
+    setObjtProduto(produto);
+    setBtnCadastrar(true);
+  }
+
+
+  const selecionarProduto = (indice) => {
+    setObjtProduto(produtos[indice]);
+    setBtnCadastrar(false);
+  }
+
+  const deletar = () => {
+    fetch("http://localhost:8080/deletar/" + objProduto.codigo, {
+      method: "delete",
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(retorno => retorno.json()).then(retorno_convertido => {
+
+        alert(retorno_convertido.mensagem);
+        let vetorTemp = [...produtos];
+        let indice = vetorTemp.findIndex((p) => {
+          return p.codigo === objProduto.codigo;
+        });
+        vetorTemp.splice(indice, 1);
+        setProdutos(vetorTemp);
+
+        limparFormulario();
+
+      })
+  }
+
+  const alterar = () => {
+    fetch("http://localhost:8080/alterar", {
+      method: "put",
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(retorno => retorno.json()).then(retorno_convertido => {
+        if (retorno_convertido.mensagem !== undefined) {
+          alert(retorno_convertido.mensagem);
+
+        } else {
+
+
+          alert('Produto alterado com sucesso');
+
+
+          let vetorTemp = [...produtos];
+          let indice = vetorTemp.findIndex((p) => {
+            return p.codigo === objProduto.codigo;
+          });
+          vetorTemp[indice] = objProduto;
+          setProdutos(vetorTemp);
+
+          limparFormulario();
+
+        }
+      })
+
+  }
+
   return (
-    <div className="App">
-      <Formulario />
-      <Tabela />
+    <div>
+      {/* <p>{JSON.stringify(objProduto)}</p> */}
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} obj={objProduto} cancelar={limparFormulario} deletar={deletar} alterar={alterar} />
+      <Tabela vetor={produtos} selecionar={selecionarProduto} />
     </div>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,15 +1,21 @@
-function Formulario() {
+function Formulario({ botao, eventoTeclado, cadastrar, obj, cancelar, deletar, alterar }) {
     return (
 
         <form>
-            <input type="text" placeholder='Nome' />
-            <input type="text" placeholder="Marca" />
+            <input type="text" value={obj.nome} onChange={eventoTeclado} name="nome" placeholder='Nome' className="form-control" />
+            <input type="text" value={obj.marca} onChange={eventoTeclado} name="marca" placeholder="Marca" className="form-control" />
 
-            <input type="button" value="Alterar" />
-            <input type="button" value="Deletar" />
-            <input type="button" value="Cancelar" />
-            <input type="button" value="Cadastrar" />
-
+            {
+                botao
+                    ?
+                    <input type="button" value="Cadastrar" onClick={cadastrar} className="btn btn-secondary" />
+                    :
+                    <div>
+                        <input type="button" value="Alterar" onClick={alterar} className="btn btn-primary" />
+                        <input type="button" value="Deletar" onClick={deletar} className="btn btn-warning" />
+                        <input type="button" value="Cancelar" onClick={cancelar} className="btn btn-danger" />
+                    </div>
+            }
         </form>
     );
 }
